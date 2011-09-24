fefd6d948fd093ccd93b827f48dae27ce6624d26
Andrei Zavada <johnhommer@gmail.com>
2011-09-25 02:06:25 +0300

    edfhed.cc: bring to life as simple EDF header viewer

--- src/tools/edfhed.cc
+++ src/tools/edfhed.cc
@@ -1,18 +1,18 @@
 // ;-*-C++-*-
 /*
- *       File name:  tools/edfed-gtk.cc
+ *       File name:  tools/edfed.cc
  *         Project:  Aghermann
  *          Author:  Andrei Zavada <johnhommer@gmail.com>
  * Initial version:  2011-07-20
  *
- *         Purpose:  EDF header editor utility (using gtk)
+ *         Purpose:  EDF header viewer
  *
  *         License:  GPL
  */
 
 
 #include "../libagh/edf.hh"
-
+#include <iostream>
 
 int
 main( int argc, char **argv)
@@ -33,11 +33,15 @@ main( int argc, char **argv)
 		return 1;
 	}
 
-	auto F = agh::CEDFFile (fname, 30);
-	;
-//	if ( chdir(wd) )
-//		;
-//	free( wd);
+	try {
+		auto F = agh::CEDFFile (fname, 30);
+		F.no_save_extra_files = true;
+
+		cout << F.details();
+	} catch (invalid_argument ex) {
+		cerr << ex.what() << endl;
+	}
+
 
 	return 0;
 }
