8b154106886f42c5bc0ba9b78ece6385526d42ff
Andrei Zavada <johnhommer@gmail.com>
2012-01-10 03:28:58 +0200

    ui/draw-signal-generic.cc: take care to truncate incomplete signal page

--- src/ui/draw-signal-generic.cc
+++ src/ui/draw-signal-generic.cc
@@ -28,16 +28,19 @@ draw_signal( const valarray<float>& signal,
 	static float* _resample_buffer = NULL;
 	static size_t _resample_buffer_size = 0;
 
+	size_t	true_length = min(end - start, signal.size() - start),
+		true_width  = width * ((double)true_length/(end - start));
+
 	if ( use_resample ) {
 		if ( _resample_buffer_size != width )
 			_resample_buffer = (float*)realloc( _resample_buffer,
 							    (_resample_buffer_size = width) * sizeof(float));
 		SRC_DATA samples;
-		samples.data_in      = const_cast<float*>(&signal[start]);
-		samples.input_frames  = end - start;
-		samples.output_frames = width;
-		samples.data_out     = _resample_buffer;
-		samples.src_ratio    = (double)samples.output_frames / samples.input_frames;
+		samples.data_in       = const_cast<float*>(&signal[start]);
+		samples.input_frames  = true_length;
+		samples.output_frames = true_width;
+		samples.data_out      = _resample_buffer;
+		samples.src_ratio     = (double)samples.output_frames / samples.input_frames;
 
 		if ( src_simple( &samples, SRC_SINC_FASTEST, 1) )
 			;
@@ -47,7 +50,7 @@ draw_signal( const valarray<float>& signal,
 			       - samples.data_out[0]
 			       * display_scale
 			       + vdisp);
-		for ( i = 0; i < width-1; ++i )
+		for ( i = 0; i < true_width-1; ++i )
 			cairo_line_to( cr, i,
 				       - samples.data_out[i]
 				       * display_scale
@@ -59,9 +62,8 @@ draw_signal( const valarray<float>& signal,
 			       - signal[start]
 			       * display_scale
 			       + vdisp);
-		size_t length = end - start;
-		for ( i = 0; i < length; ++i ) {
-			cairo_line_to( cr, ((float)i)/length * width,
+		for ( i = 0; i < true_length; ++i ) {
+			cairo_line_to( cr, ((float)i)/true_length * true_width,
 				       - signal[start + i]
 				       * display_scale
 				       + vdisp);
